657b2dc3aee3d7bee3b4c964390fac246347fc1f
aditya <69519212+Stepin104341@users.noreply.github.com>
2020-09-17 13:46:13 +0530

    Add files via upload

--- evodd.c
+++ evodd.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main()
+
+{
+
+int n;
+
+    printf("Enter an Integer Number: ");
+
+    scanf("%d",&n);
+
+if((n & 1)==1)
+
+    printf("odd");
+
+else
+
+    printf("even");
+
+}
